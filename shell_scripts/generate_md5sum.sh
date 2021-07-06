3917793dc019b6073d07ff3b2f58f939b65ffede
sunrise <BruceC95@163.com>
2021-07-06 19:57:47 +0800

    Improve generate_md5sum.sh.

--- shell_scripts/generate_md5sum.sh
+++ shell_scripts/generate_md5sum.sh
@@ -1,22 +1,27 @@
+# ***************************************************************************
+#  @brief       $module_or_feature -- brief description.
+#  @file        generate_md5sum.sh
+#  @date        2021-07-06
+#  @author      sunrise
+# ***************************************************************************
 #!/bin/sh
+help_usage() {
+    echo "usage: $0 dir_name"
+    echo "example: $0 ./images"
+}
 
-DIRECTORY_TRAGET=$1
-
-if [ -z $DIRECTORY_TRAGET ]
-then
-	DIRECTORY_TRAGET="."
+# Check input
+if [ $# -ne 1 ] || [ ! -d $1 ]; then
+    echo "parameter error!!!"
+    help_usage
+    exit 1
 fi
 
-if [ ! -d $DIRECTORY_TRAGET ]
-then
-	echo "Please input directory name as paramter!"
-fi
+# Get file name list (exclude files with ".md5sum" as the file format).
+file_list=`ls $1 | grep -v ".md5sum" | grep -v "upgrade.ini"`
 
-for file in $(ls $DIRECTORY_TRAGET)
+for file in $file_list
 do
-	if [ ! -d ${DIRECTORY_TRAGET}/${file} ] &&  ! echo $file | grep -q "md5sum" 
-#	if [ ! -d ${DIRECTORY_TRAGET}/${file} ] &&  [[ ${file} -ne "*.md5sum$" ]]
-	then
-		md5sum ${DIRECTORY_TRAGET}/${file} > ${DIRECTORY_TRAGET}/${file}.md5sum
-	fi
+    md5sum $1/$file | cut -d ' ' -f1 > $1/$file.md5sum
+    printf "finish md5sum of file:\t$file.\n"
 done
